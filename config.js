99f81b42074d1c62294f1b43bba5bb5fcfb7099a
oscarhert-bit <oscar.hert@gmail.com>
2026-01-14 19:56:25 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 /* ============================================================
    ARCHIVO DE CONFIGURACIÓN GLOBAL (Broker OS)
-   Conectado a Planilla: ...Jrek
    ============================================================ */
 
 const CONFIG = {
-    // 1. CONEXIÓN CON TU BACKEND OFICIAL
+    // 1. CONEXIÓN CON EL BACKEND (Tu URL Oficial Actualizada)
     API_URL: "https://script.google.com/macros/s/AKfycbwk8GMbobMSLlUEsDPRqzycp0JzoiNVueVE7ICN7yD27yHWbtfYVOuniUsydtydyRry/exec",
 
-    // 2. CONTACTO (Para el botón de ayuda)
-    WHATSAPP_PHONE: "56944504962", // Tu número (revisado de tus archivos anteriores)
-    WHATSAPP_MESSAGE: "Hola Oscar, tengo una duda con mi carpeta digital.",
+    // 2. CONTACTO (Para el botón de ayuda y footer)
+    WHATSAPP_PHONE: "56944504962", 
+    WHATSAPP_MESSAGE: "Hola Oscar, tengo una duda con mi evaluación.",
 
     // 3. REDIRECCIÓN
     URL_EXITO: "gracias.html" 
